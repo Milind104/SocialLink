bce86fbf96f09d3d7bc04b1c2e36e89ef599ac1f
krunal <202001442@daiict.ac.in>
2024-04-03 15:05:54 +0530

    added User custom hooks

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -41,5 +43,40 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+UserSchema.pre("save", async function(next){
+  // password is not changed
+  if(!this.isModified("password")) return next();
+
+  // password is changed or registring
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+UserSchema.methods.isPasswordCorrect = async function(password){
+  // true or false on password matching 
+  return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY});
+}
+UserSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
+}
+
 const User = mongoose.model("User", UserSchema);
+
 export default User;
