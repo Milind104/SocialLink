d0cc85b47208f847d0ee9c7ed244713384901b6b
Milind104 <202001246@daiict.ac.in>
2024-04-04 23:05:34 +0530

    Add navbar in login signup page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,21 +1,13 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./form";
+import Navbar1 from "scenes/navbar1";
 
 const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
     <Box>
-      <Box
-        width="100%"
-        backgroundColor={theme.palette.background.alt}
-        p="1rem 6%"
-        textAlign="center"
-      >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
-          SocialLink
-        </Typography>
-      </Box>
+      <Navbar1 />
 
       <Box
         width={isNonMobileScreens ? "50%" : "93%"}
